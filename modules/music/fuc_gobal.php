e85852d1d623791bf22b126fc3514eefb7334676
phantandung92 <phantandung92@57ec2516-edbd-9e4a-8475-1ab437c2aaa8>
2011-05-11 02:25:40 +0000

    Sửa đường dẫn URL
    git-svn-id: https://nukeviet-music.googlecode.com/svn/trunk@128 57ec2516-edbd-9e4a-8475-1ab437c2aaa8

--- modules/music/fuc_gobal.php
+++ modules/music/fuc_gobal.php
@@ -17,7 +17,7 @@ function get_URL( )
 	return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
 }
 
-$mainURL = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . '&' . NV_OP_VARIABLE ;
+$mainURL = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . '&amp;' . NV_OP_VARIABLE ;
 // lay thong tin the loai
 function get_category()
 {
