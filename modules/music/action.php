2b1cbb44f36bd31b58c9f55e9f77beabb41e97db
phantandung92@gmail.com <phantandung92@gmail.com@57ec2516-edbd-9e4a-8475-1ab437c2aaa8>
2011-08-09 18:37:03 +0000

    Cập nhật file action.php
    git-svn-id: https://nukeviet-music.googlecode.com/svn/trunk@145 57ec2516-edbd-9e4a-8475-1ab437c2aaa8

--- modules/music/action.php
+++ modules/music/action.php
@@ -378,6 +378,7 @@ $sql_create_module[] = "INSERT INTO  `" . $db_config['prefix'] . "_" . $lang . "
 (13, 'default_server', 1, ''),
 (14, 'playlist_max', 2, ''),
 (15, 'del_cache_time_out', 259200, ''),
-(16, 'version', 0, '3.1.02')";
+(16, 'version', 0, '3.1.02'),
+(17, 'revision', 143, '')";
 
 ?>
