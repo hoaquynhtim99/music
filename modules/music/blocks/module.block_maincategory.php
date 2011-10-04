f541e1c0d11e61ba847081d8fd826ac7ad53f2ca
phantandung92@gmail.com <phantandung92@gmail.com@57ec2516-edbd-9e4a-8475-1ab437c2aaa8>
2011-10-04 12:08:43 +0000

    Tối ưu phần chủ đề trên trang chủ
    git-svn-id: https://nukeviet-music.googlecode.com/svn/trunk@207 57ec2516-edbd-9e4a-8475-1ab437c2aaa8

--- modules/music/blocks/module.block_maincategory.php
+++ modules/music/blocks/module.block_maincategory.php
@@ -3,25 +3,24 @@
 /**
  * @Project NUKEVIET-MUSIC
  * @Author Phan Tan Dung (phantandung92@gmail.com)
- * @copyright 2011
+ * @copyright 2011 Freeware
  * @createdate 26/01/2011 09:17 AM
  */
 
 if ( ! defined( 'NV_IS_MOD_MUSIC' ) ) die( 'Stop!!!' );
 
-global $lang_module, $module_data, $module_file, $module_info, $db;
+global $lang_module, $module_data, $module_file, $module_info, $db, $module_name, $mainURL;
 
 $xtpl = new XTemplate( "block_maincategory.tpl", NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/" . $module_file );
 $xtpl->assign( 'title', $lang_module['category'] );
 
-$category = get_category() ;
+$sql = "SELECT a.cid, b.title FROM `" . NV_PREFIXLANG . "_" . $module_data . "_main_category` AS a INNER JOIN `" . NV_PREFIXLANG . "_" . $module_data . "_category` AS b ON a.cid=b.id ORDER BY a.order ASC";
+$list = nv_db_cache( $sql, 'cid', $module_name );
 
-$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data . "_main_category ORDER BY `order` ASC";
-$query = $db->sql_query( $sql );
-while( $song =  $db->sql_fetchrow( $query ) )
+foreach( $list as $row )
 {
-	$xtpl->assign( 'name', $category[$song['cid']] );
-	$xtpl->assign( 'url', $mainURL . "=search/category/" . $song['cid'] );
+	$xtpl->assign( 'name', $row['title'] );
+	$xtpl->assign( 'url', $mainURL . "=search/category/" . $row['cid'] );
 	$xtpl->parse( 'main.loop' );
 }
 
