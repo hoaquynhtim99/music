6862ce7bf80b27b4b71976cb044200f611ea5270
phantandung92@gmail.com <phantandung92@gmail.com@57ec2516-edbd-9e4a-8475-1ab437c2aaa8>
2011-10-04 12:32:12 +0000

    Tối ưu block bảng xếp hạng
    git-svn-id: https://nukeviet-music.googlecode.com/svn/trunk@208 57ec2516-edbd-9e4a-8475-1ab437c2aaa8

--- modules/music/blocks/module.block_rating_song.php
+++ modules/music/blocks/module.block_rating_song.php
@@ -3,7 +3,7 @@
 /**
  * @Project NUKEVIET-MUSIC
  * @Author Phan Tan Dung
- * @Copyright (C) 2011
+ * @Copyright (C) 2011 Freeware
  * @Createdate 26/01/2011 09:47 PM
  */
 
@@ -13,19 +13,18 @@ global $module_data, $mainURL, $db, $lang_module, $module_file;
 
 $xtpl = new XTemplate( "block_rating_song.tpl", NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/" . $module_file );
 $xtpl->assign( 'LANG', $lang_module );
-$allsinger = getallsinger();
 
-$sql = "SELECT a.songid, b.ten, b.tenthat, b.casi  FROM `" . NV_PREFIXLANG . "_" . $module_data . "_topsong` AS a INNER JOIN `" . NV_PREFIXLANG . "_" . $module_data . "` AS b ON a.songid=b.id WHERE b.active=1 ORDER BY a.hit DESC LIMIT 0,10";
-$query = $db->sql_query( $sql );
+$sql = "SELECT a.songid, b.ten, b.tenthat, b.casi, c.tenthat AS casithat  FROM `" . NV_PREFIXLANG . "_" . $module_data . "_topsong` AS a INNER JOIN `" . NV_PREFIXLANG . "_" . $module_data . "` AS b ON a.songid=b.id LEFT JOIN `" . NV_PREFIXLANG . "_" . $module_data . "_singer` AS c ON b.casi=c.ten WHERE b.active=1 ORDER BY a.hit DESC LIMIT 0,10";
+$result = $db->sql_query( $sql );
 
 $i = 1;
-while( $data =  $db->sql_fetchrow( $query ) )
+while( $data =  $db->sql_fetchrow( $result ) )
 {	
 	$xtpl->assign( 'STT', $i );
 	$xtpl->assign( 'url_view', $mainURL . "=listenone/" . $data['songid'] . "/" . $data['ten'] );
 	$xtpl->assign( 'songname', $data['tenthat'] );
 	$xtpl->assign( 'url_search_singer', $mainURL . "=search/singer/" . $data['casi'] );
-	$xtpl->assign( 'singer', $allsinger[$data['casi']] );
+	$xtpl->assign( 'singer', empty( $data['casithat'] ) ? $lang_module['unknow'] : $data['casithat'] );
 	$xtpl->parse( 'main.loop' );
 	$i++;
 }
