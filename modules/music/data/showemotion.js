5d2ef93006e2887edcd126b55e69601bafcf5c1e
phantandung92@gmail.com <phantandung92@gmail.com@57ec2516-edbd-9e4a-8475-1ab437c2aaa8>
2011-10-15 11:18:01 +0000

    Nâng cấp chức năng biểu tương cảm xúc
    git-svn-id: https://nukeviet-music.googlecode.com/svn/trunk@236 57ec2516-edbd-9e4a-8475-1ab437c2aaa8

--- modules/music/data/showemotion.js
+++ modules/music/data/showemotion.js
@@ -4,14 +4,86 @@
  * @Createdate 26 - 12 - 2010 5 : 12
  */
 
+var clientPC = navigator.userAgent.toLowerCase(); // Get client info
+var is_ie = ((clientPC.indexOf('msie') != -1) && (clientPC.indexOf('opera') == -1));
+var baseHeight;
+
+if (is_ie && typeof(baseHeight) != 'number')
+{
+	baseHeight = document.selection.createRange().duplicate().boundingHeight;
+}
+
 $(function(){
 	$("#showemotion").click(function(){
 		$("#emotion").show();
 	});
 	$("#emotion a").click(function(){
-		var value = $("#commentbody").val();
 		var add = $(this).attr("title");
-		$("#commentbody").val( value + add);
+		nvm_insert_text(add, true);
 		$("#emotion").hide();
 	});
 });
+
+// Chen doan ma vao
+function nvm_insert_text(text, spaces)
+{
+	if (spaces) {
+		text = ' ' + text + ' ';
+	}
+	
+	var textarea = document.getElementById('commentbody');
+
+	if (!isNaN(textarea.selectionStart))
+	{
+		var sel_start = textarea.selectionStart;
+		var sel_end = textarea.selectionEnd;
+		mozWrap(textarea, text, '');
+		textarea.selectionStart = sel_start + text.length;
+		textarea.selectionEnd = sel_end + text.length;
+	}
+	else if (textarea.createTextRange && textarea.caretPos)
+	{
+		if (baseHeight != textarea.caretPos.boundingHeight) 
+		{
+			textarea.focus();
+			storeCaret(textarea);
+		}
+		var caret_pos = textarea.caretPos;
+		caret_pos.text = caret_pos.text.charAt(caret_pos.text.length - 1) == ' ' ? caret_pos.text + text + ' ' : caret_pos.text + text;
+	}
+	else
+	{
+		textarea.value = textarea.value + text;
+	}
+
+	textarea.focus();
+}
+
+function mozWrap(txtarea, open, close)
+{
+	var selLength = (typeof(txtarea.textLength) == 'undefined') ? txtarea.value.length : txtarea.textLength;
+	var selStart = txtarea.selectionStart;
+	var selEnd = txtarea.selectionEnd;
+	var scrollTop = txtarea.scrollTop;
+	if (selEnd == 1 || selEnd == 2) 
+	{
+		selEnd = selLength;
+	}
+	var s1 = (txtarea.value).substring(0,selStart);
+	var s2 = (txtarea.value).substring(selStart, selEnd);
+	var s3 = (txtarea.value).substring(selEnd, selLength);
+	txtarea.value = s1 + open + s2 + close + s3;
+	txtarea.selectionStart = selStart + open.length;
+	txtarea.selectionEnd = selEnd + open.length;
+	txtarea.focus();
+	txtarea.scrollTop = scrollTop;
+	return;
+}
+
+function storeCaret(textEl)
+{
+	if (textEl.createTextRange)
+	{
+		textEl.caretPos = document.selection.createRange().duplicate();
+	}
+}
