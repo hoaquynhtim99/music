aff2b290d5a1062c7d0d97bf8c407c9e30494c07
phantandung92 <phantandung92@57ec2516-edbd-9e4a-8475-1ab437c2aaa8>
2012-01-02 12:10:01 +0000

    Thêm phần tìm kiếm cho phần quản lý quà tặng âm nhạc
    git-svn-id: https://nukeviet-music.googlecode.com/svn/trunk@257 57ec2516-edbd-9e4a-8475-1ab437c2aaa8

--- modules/music/admin/gift.php
+++ modules/music/admin/gift.php
@@ -1,18 +1,26 @@
 <?php
 
 /**
- * @Project NUKEVIET 3.0
- * @Author VINADES.,JSC (contact@vinades.vn)
- * @Copyright (C) 2010 VINADES.,JSC. All rights reserved
+ * @Project NUKEVIET-MUSIC
+ * @Author PHAN TAN DUNG (phantandung92@gmail.com)
+ * @Copyright (C) 2010 Freeware
  * @Createdate 9-8-2010 14:43
  */
-if ( ! defined( 'NV_IS_MUSIC_ADMIN' ) ) die( 'Stop!!!' );
-$page_title = $lang_module['list_gift'];
-$contents = '' ;
 
-// lay du lieu 
+if ( ! defined( 'NV_IS_MUSIC_ADMIN' ) ) die( 'Stop!!!' );
+
+$page_title = $lang_module['list_gift'];
+
+// Call jquery datepicker + shadowbox
+$my_head .= "<link type=\"text/css\" href=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.core.css\" rel=\"stylesheet\" />\n";
+$my_head .= "<link type=\"text/css\" href=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.theme.css\" rel=\"stylesheet\" />\n";
+$my_head .= "<link type=\"text/css\" href=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.datepicker.css\" rel=\"stylesheet\" />\n";
+
+$my_head .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.core.min.js\"></script>\n";
+$my_head .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/ui/jquery.ui.datepicker.min.js\"></script>\n";
+$my_head .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . "js/language/jquery.ui.datepicker-" . NV_LANG_INTERFACE . ".js\"></script>\n";
+
 $now_page = $nv_Request->get_int( 'now_page', 'get', 0 );
-$link = "index.php?" . NV_NAME_VARIABLE . "=" . $module_name."&op=gift" ;
 
 if (!$now_page) 
 {
@@ -24,57 +32,103 @@ else
 	$first_page = ($now_page -1)*100;
 }	
 
+$sql = "FROM `" . NV_PREFIXLANG . "_" . $module_data . "_gift` AS a INNER JOIN `" . NV_PREFIXLANG . "_" . $module_data . "` AS b ON a.songid=b.id WHERE a.id!=0";
+$link = NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_NAME_VARIABLE . "=" . $op;
 
-// tinh so trang
-$num_page = "SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data."_gift" ;
-$num = mysql_query($num_page);
-$output = mysql_num_rows($num);
+// Search data
+$data_search = array(
+	"q" => filter_text_input( 'q', 'get', $lang_module['filter_enterkey'], 1, 100 ), //
+	"from" => filter_text_input( 'from', 'get', '', 1, 100 ), //
+	"to" => filter_text_input( 'to', 'get', '', 1, 100 ), //
+	"disabled" => " disabled=\"disabled\"" //
+);
+
+// Enable cancel filter data
+if ( ( $data_search['q'] != $lang_module['filter_enterkey'] and ! empty ( $data_search['q'] ) ) or ! empty ( $data_search['from'] ) or ! empty ( $data_search['to'] ) )
+{
+	$data_search['disabled'] = "";
+}
+
+if ( ! empty ( $data_search['q'] ) and $data_search['q'] != $lang_module['filter_enterkey'] )
+{
+	$link .= "&amp;q=" . $data_search['q'];
+	$sql .= " AND ( a.body LIKE '%" . $db->dblikeescape( $data_search['q'] ) . "%' OR b.tenthat LIKE '%" . $db->dblikeescape( $data_search['q'] ) . "%' )";
+}
+
+if ( ! empty ( $data_search['from'] ) )
+{
+	unset( $match );
+	if ( preg_match( "/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/", $data_search['from'], $match ) )
+	{
+		$from = mktime( 0, 0, 0, $match[2], $match[1], $match[3] );
+		$sql .= " AND a.time >= " . $from;
+		$link .= "&amp;from=" . $data_search['from'];
+	}
+}
+	
+if ( ! empty ( $data_search['to'] ) )
+{
+	unset( $match );
+	if ( preg_match( "/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/", $data_search['to'], $match ) )
+	{
+		$to = mktime( 0, 0, 0, $match[2], $match[1], $match[3] );
+		$sql .= " AND a.time <= " . $to;
+		$link .= "&amp;to=" . $data_search['to'];
+	}
+}
+
+$sql .= " ORDER BY a.id DESC";
+
+// Get num row
+$sql1 = "SELECT COUNT(*) " . $sql;
+$result1 = $db->sql_query( $sql1 );
+list( $output ) = $db->sql_fetchrow( $result1 );
 $ts = 1;
 while ( $ts * 100 < $output ) {$ts ++ ;}
 
-// ket qua
 $xtpl = new XTemplate("gift.tpl", NV_ROOTDIR . "/themes/" . $global_config['module_theme'] . "/modules/" . $module_name);
 $xtpl->assign('LANG', $lang_module);
 $xtpl->assign('URL_DEL_BACK', $link);
 $xtpl->assign('URL_ACTIVE_LIST', "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=listactive&where=_gift" );
 $xtpl->assign('URL_DEL', "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=delall&where=_gift");
 
-$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data . "_gift ORDER BY ID DESC LIMIT ".$first_page.",100";
-$result = mysql_query( $sql );
+$xtpl->assign( 'NV_BASE_ADMINURL', NV_BASE_ADMINURL );
+$xtpl->assign( 'NV_NAME_VARIABLE', NV_NAME_VARIABLE );
+$xtpl->assign( 'NV_OP_VARIABLE', NV_OP_VARIABLE );
+$xtpl->assign( 'MODULE_NAME', $module_name );
+$xtpl->assign( 'OP', $op );
+
+$xtpl->assign( 'DATA_SEARCH', $data_search );
+$xtpl->assign( 'URL_CANCEL', NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=" . $op );
 
 $link_del = "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=del";
 $link_active = "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=active&where=_gift&id=";
 $link_edit = "index.php?" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=editgift";
 
-while($rs = $db->sql_fetchrow($result))
+$sql = "SELECT a.*, b.tenthat " . $sql . " LIMIT " . $first_page . ", 50";
+$result = $db->sql_query( $sql );
+
+while($row = $db->sql_fetchrow($result))
 {	
-	$body = substr($rs['body'], 0, 94) ;
-	$xtpl->assign('id', $rs['id']);
-	$xtpl->assign('body', $body);
-	$xtpl->assign('name', $rs['who_send']);
-	
-	$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data . " WHERE id=".$rs['songid']." LIMIT 0,1";
-	$resultsong = mysql_query( $sql );	
-	while ($song = $db->sql_fetchrow($resultsong))
-	{
-		$songname = $song['tenthat'];
-	}
-	$xtpl->assign('song', $songname);
+	$xtpl->assign('id', $row['id']);
+	$xtpl->assign('body', nv_clean60( strip_tags( $row['body'] ), 200 ));
+	$xtpl->assign('name', $row['who_send']);
+	$xtpl->assign('song', $row['tenthat']);
 	
 	$class = ($i % 2) ? " class=\"second\"" : "";
 	$xtpl->assign('class', $class);
-	$xtpl->assign('URL_DEL_ONE', $link_del . "&where=_gift&id=" . $rs['id']);
-	$xtpl->assign('URL_EDIT', $link_edit . "&id=" . $rs['id']);
+	$xtpl->assign('URL_DEL_ONE', $link_del . "&where=_gift&id=" . $row['id']);
+	$xtpl->assign('URL_EDIT', $link_edit . "&id=" . $row['id']);
 	
-	$str_ac = ($rs['active'] == 1) ? $lang_module['active_yes'] : $lang_module['active_no'];
+	$str_ac = ($row['active'] == 1) ? $lang_module['active_yes'] : $lang_module['active_no'];
 	$xtpl->assign('active', $str_ac);
-	$xtpl->assign('URL_ACTIVE', $link_active . $rs['id']);
+	$xtpl->assign('URL_ACTIVE', $link_active . $row['id']);
 	
 	$xtpl->parse('main.row');
 }
 
 $xtpl->parse('main');
-$contents .= $xtpl->text('main');
+$contents = $xtpl->text('main');
 $contents .= "<div align=\"center\" style=\"width:300px;margin:0px auto 0px auto;\">\n ";
 $contents .= new_page_admin( $ts, $now_page, $link);
 $contents .= "</div>\n";
