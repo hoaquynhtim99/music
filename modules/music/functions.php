81aefe7083b5d4c97724cd511e8cda70b6a81d28
Phan Tan Dung <phantandung92@gmail.com>
2017-03-20 03:46:47 +0700

    Thêm block view_singer_header

--- modules/music/functions.php
+++ modules/music/functions.php
@@ -74,8 +74,8 @@ $global_array_config['op_alias_prefix'] = array(
 $global_array_config['gird_albums_percat_nums'] = 12;
 $global_array_config['gird_albums_incat_nums'] = 24;
 
-$global_array_config['view_singer_show_header'] = true;
-$global_array_config['view_singer_headtext_length'] = 200;
+$global_array_config['view_singer_show_header'] = false;
+$global_array_config['view_singer_headtext_length'] = 240;
 $global_array_config['view_singer_tabs_alias'] = array(
     'song' => 'bai-hat',
     'video' => 'video',
