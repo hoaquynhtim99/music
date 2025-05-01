88bc2f1b0121bba725ebc8327fc510a7e1860383
Phan Tấn Dũng <phantandung92@gmail.com>
2025-05-01 20:01:34 +0700

    Thay đổi header các file

--- src/modules/music/Album/Album.php
+++ src/modules/music/Album/Album.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * @Project NUKEVIET MUSIC 4.X
+ * @Project NUKEVIET MUSIC 5.X
  * @Author PHAN TAN DUNG <writeblabla@gmail.com>
- * @Copyright (C) 2016 PHAN TAN DUNG. All rights reserved
+ * @Copyright (C) 2016-2025 PHAN TAN DUNG. All rights reserved
  * @License GNU/GPL version 2 or any later version
- * @Createdate Sun, 26 Feb 2017 14:04:32 GMT
+ * @Createdate Thursday, May 1, 2025 7:59:37 PM GMT+07:00
  */
 
 namespace NukeViet\Module\music\Album;
